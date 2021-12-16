fbfc4782e1b507bee20f3262277f1b11976c0bea
Chocolate <amyyip@maceducationcentre.com>
2021-12-16 22:46:54 +0800

    portfolio

---
+++ Homework Day 6/Preface-JS-basics/assignments/index.js
@@ -0,0 +1,78 @@
+/** ASSIGNMENT */
+
+/** NUMBER */
+// 1. Calculate your age in seconds
+const a = "age";
+age[].getMinutes()
+
+console.log("hi")
+
+/** STRING */
+// 2. Format this string (Hint: use `split`)
+let messyString = "dO yOu kNOw wHat i mEAn?";
+
+// 3. Get the second character of each string (Hint: use index)
+let string1 = "tYpe";
+let string2 = "Some";
+let string3 = "Numbers";
+let string4 = "?!";
+
+let output; //TODO
+
+// 4. Create an echo effect. Eg. "PREFACE... Preface... preface... "
+function echo(string) {
+    // TODO
+    console.log("");
+}
+
+// DO NOT EDIT this line
+echo("preface"); // should console.log "PREFACE... Preface... preface... "
+
+// 5. Find if stringA is a substring of stringB. For example, "can" is a substring of "candle" (Hint: consider case and look at `includes` string method)
+function isSubstring(stringA, stringB) {
+    // TODO
+    return;
+}
+
+// DO NOT EDIT this line
+console.log(isSubstring("can", "candle")); // should return true
+console.log(isSubstring("can", "handle")); // should return false
+
+// 6. (HARD) Find the missing letter in the passed letter range and return it. If all letters are present, return undefined (Hint: upper bound is start of the string, lower bound is end of the string.)
+function missingLetters(string) {
+    // TODO
+    return;
+}
+
+missingLetters("abce"); // "d"
+missingLetters("abcdefghjklmno"); // "i"
+missingLetters("abcdefghijklmnopqrstuvwxyz"); // undefined
+
+// 7. (HARD) Format dates to this format "1/1/2022" (Hint: use an object, use `split`)
+let testDate1 = "Jan 1, 2022";
+let testDate2 = "Jan 01, 2022";
+let testDate3 = "January 1, 2022";
+let testDate4 = "January 01, 2022";
+
+/** ARRAY */
+// 8. Choose a random response from this array of phrases. (Hint: Look at `length`, `Math.floor` and `Math.random`)
+let phrases = ["Sure thing", "That sounds good", "Yes definitly", "Maybe not today", "Computer says no"];
+
+// 9. Fill in the missing numbers for this array. Expect to get back [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let numbers = [0, 2, 3, 5, 7, 9];
+
+/** OBJECT */
+let menu = {};
+// 10. How do you check if the menu is empty? (Hint: `Object.keys` or `Object.values`)
+
+// 11. Fill in the menu with appetizer, main, desert, drink. Think what should the values should be. (Hint: It should have prices per item)
+
+// 12. Create an order from the menu. Calculate your total.
+let myOrder;
+let iOwe;
+
+// 13. Un-order something from your order.
+
+// 14. Restaurant has a promotion. Change the prices of some items on the menu. Check if your order's total has changed.
+
+// 15. Find the total number of food on the menu (appetizer + main + desert + drink)
---
+++ Homework Day 6/Preface-JS-basics/index.js
@@ -0,0 +1,21 @@
+/**
+ * Hey all! 
+ * 
+ * We don't have class this week. Please review the notes from last class in the notes folder.
+ * 
+ * Make sure you're comfortable with basic CRUD methods of these data types.
+ * 
+ * Then try to do the questions in the assignments folder.
+ * 
+ * Here's how I would work on the assignments: 
+ * 1) Open a new Replit tab and copy the assignments to its `index.js` or
+ * 2) Press "Fork" in Replit. This should create a copy in your Replit account. Work on the questions in the `index.js` file or 
+ * 3) Download this repo as zip folder (third button next to Files) and open it in VSCode. Then work on the questions in Quokka.
+ * 
+ * Have fun and Good Luck! 
+ * 
+ * See you next week!
+ * 
+ * Harry
+ * 
+ */
